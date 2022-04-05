ab569c9b6efd7e422004e93a2c7c072dd4b5f1eb
sahil-vandra <98399594+sahil-vandra@users.noreply.github.com>
2022-04-05 19:45:32 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-echo "You are in install_dependencies.sh file"
-sudo yum update
-sudo yum -y install python-pip
-sudo pip install virtualenv
+# sudo yum update
+# sudo yum -y install python-pip
+# sudo pip install virtualenv
+# cd /home/ec2-user
+# virtualenv environment
+# source environment/bin/activate
+# sudo pip install -r requirements.txt
+# sudo pip install Django==1.11.15
+
+yum update
+easy_install pip
+pip install virtualenv
 cd /home/ec2-user
 virtualenv environment
 source environment/bin/activate
-sudo pip install -r requirements.txt
-sudo pip install Django==1.11.15
-
+pip install -r requirements.txt
